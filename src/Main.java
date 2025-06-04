00e0b29717e167cefbf9c5799db45e744a62b306
YaroslavSE <smaglijaroslav4@gmail.com>
2025-06-04 22:19:51 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,21 @@ import java.util.concurrent.Semaphore;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private Semaphore access;
+    private Semaphore access = new Semaphore(1);
     private Semaphore full;
-    private Semaphore empty;
+    private Semaphore empty = new Semaphore(0);
 
     private ArrayList<String> storage = new ArrayList<>();
 
 
     public static void main(String[] args) {
         Main program = new Main();
-        program.starter(3,15,2,4);
+        //program.starter(3,15,2,4);
+        program.fixedStarter(3);
     }
 
     private void starter(int storageSize, int totalItems, int producerCount, int consumerCount){
-        access = new Semaphore(1);
         full = new Semaphore(storageSize);
-        empty = new Semaphore(0);
 
         int itemsPerProducer = totalItems / producerCount;
         int itemsPerConsumer = totalItems / consumerCount;
@@ -28,14 +27,12 @@ public class Main {
         int remainingProducerItems = totalItems % producerCount;
         int remainingConsumerItems = totalItems % consumerCount;
 
-        ArrayList<Thread> threads = new ArrayList<Thread>();
 
         for(int i = 0; i < producerCount; i++){
             int itemsToProduce = itemsPerProducer + (i < remainingProducerItems ? 1 : 0);
             int producerId = i + 1;
             Thread t = new Thread(() -> producer(itemsToProduce, producerId));
             t.start();
-            threads.add(t);
         }
 
         for (int i = 0; i < consumerCount; i++)
@@ -44,16 +41,29 @@ public class Main {
             int consumerId = i + 1;
             Thread t = new Thread(() -> consumer(itemsToConsume, consumerId));
             t.start();
-            threads.add(t);
         }
 
-        for (Thread t : threads) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.out.println("Thread interrupted.");
-            }
+
+    }
+    private void fixedStarter(int storageSize){
+        full = new Semaphore(storageSize);
+
+        int[] producersLoad = {5,10,15};
+        int[] consumersLoad = {4, 4, 8, 10, 2, 2};
+
+        for(int i = 0; i < producersLoad.length; i++){
+            int itemsToProduce = producersLoad[i];
+            int producerId = i + 1;
+            Thread t = new Thread(() -> producer(itemsToProduce, producerId));
+            t.start();
+        }
+
+        for (int i = 0; i < consumersLoad.length; i++)
+        {
+            int itemsToConsume = consumersLoad[i];
+            int consumerId = i + 1;
+            Thread t = new Thread(() -> consumer(itemsToConsume, consumerId));
+            t.start();
         }
     }
 
@@ -65,7 +75,7 @@ public class Main {
 
                 String item = "item P" + producerId + "-" + (i + 1);
                 storage.add(item);
-                System.out.println("[Producer " + producerId + "] Produced: " + item + "storage: " + storage.size()) ;
+                System.out.println("[Producer " + producerId + "] Produced: " + item + " storage: " + storage.size()) ;
 
                 access.release();  // вихід зі сховища
                 empty.release();   // новий елемент доступний
@@ -81,12 +91,12 @@ public class Main {
                 access.acquire();   // вхід до сховища
 
                 String item = storage.remove(0);
-                System.out.println("[Consumer " + consumerId + "] Consumed: " + item + "storage: " + storage.size());
+                System.out.println("[Consumer " + consumerId + "] Consumed: " + item + " storage: " + storage.size());
 
                 access.release();   // вихід зі сховища
                 full.release();     // звільнили місце
 
-                Thread.sleep(3000); // імітація обробки
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
